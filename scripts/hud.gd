6bc9197ca8fde4f03ec77d7ccc4f686210542111
Baunsu <zdixon008@gmail.com>
2024-12-22 23:36:41 -0800

    game over timer

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
 signal start_game
-
+signal end_game
+	
+func update_time():
+	$Time.text = str(ceil($TimeLimit.time_left))
+	
 func show_message(text):
 	$Message.text = text
 	$Message.show()
@@ -15,7 +19,7 @@ func show_game_over():
 	$Message.show()
 	
 	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
+	$Start.show()
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
@@ -26,3 +30,6 @@ func _on_start_pressed() -> void:
 
 func _on_message_timer_timeout() -> void:
 	$Message.hide()
+
+func _on_time_limit_timeout() -> void:
+	end_game.emit()
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,13 +4,15 @@ extends Node
 var baachi
 
 func _ready() -> void:
-    pass
+    $Player.hide()
     
 func _process(delta: float) -> void:
+    $HUD.update_time()
     $HUD.update_score(Global.score)
     
 func game_over():
     $BaachiTimer.stop()
+    $HUD/TimeLimit.stop()
     $HUD.show_game_over()
     
 func new_game():
@@ -22,6 +24,7 @@ func new_game():
     
 func add_score(points):
     $HUD.update_score(points)
+    $HUD/TimeLimit.get_wait_time()
     
 func _on_baachi_timer_timeout() -> void:
     baachi = mob_scene.instantiate()
@@ -56,3 +59,4 @@ func _on_baachi_timer_timeout() -> void:
 
 func _on_start_timer_timeout() -> void:
     $BaachiTimer.start()
+    $HUD/TimeLimit.start()
